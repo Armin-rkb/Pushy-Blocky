631381753f181ecc435048b331f46d3668f26dba
Armin Karimi Birgani <armin.rkb@gmail.com>
2022-09-04 13:06:00 +0200

    Start of Project. [Add] InputManager package. Player Controller with basic ball movement.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    void StartGame()
+    {
+
+    }
+
+    void PauseGame()
+    {
+        
+    }
+
+    void ResumeGame()
+    {
+        
+    }
+
+    void EndGame()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rb;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float maxForce;
+    private Vector2 moveDir;
+    private Vector2 baseMoveDir = new Vector2(0, 0.25f);
+
+    public void OnMove(InputAction.CallbackContext context) 
+    {
+        moveDir = context.ReadValue<Vector2>();
+    }
+
+    public void OnPause(InputAction.CallbackContext context)
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 currentVelocity = rb.velocity;
+        
+        // Get our new direction.
+        Vector2 newMoveDir = baseMoveDir + moveDir;
+        Mathf.Clamp(moveDir.x, -1, 1);
+        Mathf.Clamp(moveDir.y, -1, 1);
+
+        // Set the target velocity to move towards.
+        Vector3 targetVelocity = new Vector3(newMoveDir.x, 0, newMoveDir.y);
+        targetVelocity *= speed;
+
+        Vector3 velocityChange = (targetVelocity - currentVelocity);
+        Vector3.ClampMagnitude(velocityChange, maxForce);
+
+        // Add force to the changed velocity.
+        rb.AddForce(velocityChange, ForceMode.Impulse);
+    }
+}
